c99aa5a512cca741601958e97e8eb9a16cab7755
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-12-04 01:38:11 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -98,7 +98,7 @@ bot.on('ready', () => {
 
 bot.on('message', (message) => {
   if (message.author.id === bot.user.id) return;
-  if (message.content.substring(0, 4) !== 'shade!') return;
+  if (message.content.substring(0, 4) !== 'cat!') return;
 
   const commandObject = parseMessage(message);
   if (commandObject) {
